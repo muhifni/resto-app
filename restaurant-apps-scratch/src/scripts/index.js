6abfad27e8ac7732bf35939d53448dffdb75049f
muhifni <muhammad.gcs@gmail.com>
2021-12-11 23:34:47 +0800

    memperbaiki struktur index.html dan memperbaiki cara penyajian file css

--- restaurant-apps-scratch/src/scripts/index.js
+++ restaurant-apps-scratch/src/scripts/index.js
@@ -4,6 +4,7 @@ import "../styles/navbar.css";
 import "../styles/hero.css";
 import "../styles/content.css";
 import "../styles/footer.css";
+import "../styles/card.css";
 import "../scripts/components/card";
 import { changeColor, navbar } from "./navbar";
 
